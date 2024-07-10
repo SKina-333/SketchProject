0d330d84283da7f08f811e788d3019b5dab1df05
Sokina Ho <shoo0042@student.monash.edu>
2024-07-11 00:02:00 +1000

    complete the hover effect now just need to make it better

--- javascript.js
+++ javascript.js
@@ -4,12 +4,18 @@ const container = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", function(e){
     createPixels(16);
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover", function(){
+            pixel.style.backgroundColor = "blue";
+        });
+    });
 });
 
 function createPixels(pixels) {
     let pixelCount = rowCount = parseInt(pixels)*1;
-    const pixelWidth = container.style.width / pixels + "px";
-    const pixelHeight = container.style.height / pixels + "px";
+    
     while (rowCount>0){
         let newRow = document.createElement("div");
        
@@ -20,15 +26,20 @@ function createPixels(pixels) {
         newRow.style.backgroundColor = "white"
         newRow.style.display = "flex";
         newRow.style.flexDirection = "row";
+        newRow.style.justifyContent = "space-between";
+        newRow.style.border = "1px solid grey";
+        
+
         container.appendChild(newRow);
         while (pixelCount>0){    
             let newDiv = document.createElement("div");
             newDiv.classList.add("pixel");
             newDiv.id = "pixel-"+(Math.abs(pixels-rowCount)+1)+"-"+(Math.abs(pixels-pixelCount)+1);
             newDiv.style.backgroundColor = "white";
-            newDiv.style.width =  "100px";
+            newDiv.style.width =  "100%";
             newDiv.style.height = "100%";
-            newDiv.style.border = "0.5px solid black";
+            newDiv.style.backgroundColor = "rgba(220, 20, 60, 0)";
+            newDiv.style.border = "1px solid grey";
             
             newRow.appendChild(newDiv);
             pixelCount -= 1;
@@ -38,9 +49,17 @@ function createPixels(pixels) {
     }
     container.style.display = "flex";
     container.style.flexDirection = "column";
+    container.style.alignItems = "center";
+    container.style.border = "solid 5px black";
     
-    
-
 }
+
+
+
+
+function setColor(){
+    console.log("setColor");
+}
+
 
 
